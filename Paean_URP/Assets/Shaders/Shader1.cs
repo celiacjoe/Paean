98183a5e8cbb8f15a8c58e19c847fcdd0d611aac
Louis Cortes <cortes.lou@gmail.com>
2021-10-09 22:46:45 +0200

    shader01

---
+++ Paean_URP/Assets/Shaders/Shader1.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Shader1 : MonoBehaviour
+{
+    public ComputeShader compute_shader;
+    RenderTexture A;
+    public GameObject img1;
+    public int resx;
+    public int resy;
+    int handle_main;
+    public Texture texture0;
+    public Texture texture1;
+    void Start()
+    { 
+  
+        A = new RenderTexture(resx, resy, 0);
+        A.enableRandomWrite = true;
+        A.Create();
+        handle_main = compute_shader.FindKernel("CSMain");
+    }
+   
+    void Update()
+    {
+       
+
+        compute_shader.SetTexture(handle_main, "reader", A);
+        compute_shader.SetTexture(handle_main, "texture0", texture0);
+        compute_shader.SetTexture(handle_main, "texture1", texture1);
+        compute_shader.SetFloat("_time", Time.time);
+        compute_shader.SetInt("_rx", resx);
+        compute_shader.SetInt("_ry", resy);
+        //compute_shader.SetTexture(handle_main, "writer", B);
+        //compute_shader.Dispatch(handle_main, B.width / 8, B.height / 8, 1);
+        //compute_shader.SetTexture(handle_main, "reader", B);
+        //compute_shader.SetTexture(handle_main, "writer", A);
+        compute_shader.Dispatch(handle_main, A.width / 8, A.height / 8, 1);
+        img1.GetComponent<Renderer>().material.mainTexture = A;
+
+    }
+  
+}
---
+++ Paean_URP/Assets/Shaders/detec.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+public class detec : MonoBehaviour
+{
+    public ComputeShader compute_shader;
+    RenderTexture A;
+    RenderTexture B;
+
+    public GameObject img1;
+    
+    //public Material material;
+    int handle_main;
+    /*[Range(0, 1)]
+    public float mousex;   */
+    [Range(0, 1)]
+    public float mousey;
+
+    void Start()
+    { 
+  
+        A = new RenderTexture(Screen.width, Screen.height, 0);
+        A.enableRandomWrite = true;
+        A.Create();
+        B = new RenderTexture(Screen.width, Screen.height, 0);
+        B.enableRandomWrite = true;
+        B.Create();
+        handle_main = compute_shader.FindKernel("CSMain");
+    }
+   
+    void Update()
+    {
+       
+
+        compute_shader.SetTexture(handle_main, "reader", A);
+        compute_shader.SetFloat("_time", Time.time);
+        compute_shader.SetFloat("_rx", Screen.width);
+        compute_shader.SetFloat("_ry", Screen.height);
+        //compute_shader.SetFloat("_mousex",mousex);
+        compute_shader.SetFloat("_mousey", mousey);
+        compute_shader.SetTexture(handle_main, "writer", B);
+        compute_shader.Dispatch(handle_main, B.width / 8, B.height / 8, 1);
+        compute_shader.SetTexture(handle_main, "reader", B);
+        compute_shader.SetTexture(handle_main, "writer", A);
+        compute_shader.Dispatch(handle_main, B.width / 8, B.height / 8, 1);
+        img1.GetComponent<Renderer>().material.mainTexture = B;
+
+    }
+  
+}
